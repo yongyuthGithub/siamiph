818692bffed20c1059de5aa673bbe03e1580dd22
Yongyuth (Private) <yongyuth_2527@hotmail.com>
2018-05-07 13:50:31 +0700

    Add Pemission Function

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
 
-        <title>บริษัท พราว</title>
+        <title>ห้างหุ้นส่วนจำกัด สยาม ไอพีเอช</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
